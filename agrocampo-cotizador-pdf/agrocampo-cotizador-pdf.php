b05b2a89762758b8df496f638c62c17ac0bd237e
Jonianc <joni-anc@hotmail.com>
2026-02-13 03:21:22 -0300

    test: trigger hook (2)

--- agrocampo-cotizador-pdf/agrocampo-cotizador-pdf.php
+++ agrocampo-cotizador-pdf/agrocampo-cotizador-pdf.php
@@ -132,3 +132,4 @@ function acpdf_deactivate() {
 register_deactivation_hook(__FILE__, 'acpdf_deactivate');
 // hook test Fri Feb 13 03:14:13 HSP 2026
 // hook test Fri Feb 13 03:19:20 HSP 2026
+// hook test Fri Feb 13 03:21:21 HSP 2026
